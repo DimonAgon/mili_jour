bbe49789e0b7c2496d14c992e940648cff4cf263
Dimon <dimon2003agon@gmail.com>
2022-10-20 19:10:24 +0300

    add changes

--- mili_jourBot/bot/handlers/handlers.py
+++ mili_jourBot/bot/handlers/handlers.py
@@ -1,10 +1,10 @@
 from aiogram import types
 from .dispatcher import dp
 from ..models import *
-
+from datetime import date
 
 @dp.message_handler(
-    commands=['start', 'last', 'on_date']
+    commands=['start', 'last', 'on_date', 'new_journal', 'presence_reco']
 )
 
 async def start(message: types.Message):
@@ -15,15 +15,15 @@ async def start(message: types.Message):
 
     await message.reply(mess)
 
+async def new_journal(message: types.Message, squadron):
+    pass
 
-@dp.poll_handler(
-    commands=['new_journal']
-)
-async def new_journal(squadron, external_id):
-    Journal(name=squadron, external_id=external_id)
+async def presence_reco(message:types.Message):
+    today = date.today()
+    await message.answer_poll(question=today, options=["Так", "Ні"], type='quiz', correct_option_id=1, is_anonymous=False)
+    JournalEntry(journal=)
 
-#
-# @dp.answer_handler(comands=['presence_reco'])
-# async def presence_reco():
-#     pass
-#
+async def on_date(message:types.Message, requested_date):
+    await message.reply()
+
+
--- mili_jourBot/bot/models.py
+++ mili_jourBot/bot/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 class Profile(models.Model):
     external_id = models.CharField(verbose_name="тег", max_length=65)
     name = models.CharField(verbose_name="Ім'я, Прізвище", max_length=65)
-    Journal = models.CharField(verbose_name="Журнал", max_length=65)
+    journal_id = models.CharField(verbose_name="Журнал", max_length=65)
 
     def get_absolute_url(self):
         return reverse_lazy('profile', kwargs={'news_id': self.pk})
@@ -22,18 +22,4 @@ class Profile(models.Model):
 class JournalEntry(models.Model):
     date = models.DateField()
     name = models.ForeignKey(to='Profile', on_delete=models.CASCADE)
-    journal = models.ForeignKey(to='Journal', on_delete=models.CASCADE)
-
-class Journal(models.Model):
-    name = models.PositiveIntegerField(verbose_name="Номер взводу")
-    external_id = models.PositiveIntegerField()
-
-    def get_absolute_url(self):
-        return reverse_lazy('journal', kwargs={'news_id': self.pk})
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "Журнал"
-        ordering = ['-name']
+    journal_id = models.CharField()
