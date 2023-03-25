bff6953beea0f4b49bea83bff6a558f43b9a7365
DimonAgon <dimon2003agon@gmail.com>
2023-03-25 11:09:11 +0200

    implement aiogram-froms

--- mili_jourBot/bot/forms.py
+++ mili_jourBot/bot/forms.py
@@ -1,23 +1,47 @@
-
 from django import forms
 from .models import Profile
 from django.core import exceptions
-import re
+from aiogram.filters.state import State, StatesGroup
+from aiogram_forms import Form, fields, dispatcher
+import regex
 
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['name', 'journal', 'ordinal', 'external_id']
 
-        labels = {
-            'name': "Ваше Прізвище та Ім'я",
-            'ordinal': "Ваш номер у списку"
-        }
+@dispatcher.register('profileform')
+class ProfileForm(Form):
+    name = fields.TextField(label="Ваше Прізвище та Ім'я", validators=['checkname'])
+    journal = fields.TextField(label="Номер вашого взводу")
+    ordinal = fields.TextField(label="Ваш номер у списку")
 
-        def clean_name(self):
-            name = self.cleaned_data['name']
-            if re.match(pattern="\p{Lu}{1}\p{L}+(?:[- ])?\p{L}*\s\p{Lu}{1}\p{L}+", string=name):
-                raise exceptions.ValidationError("Введіть ім'я коректно")
-            return name
 
+def check_name(name):
+
+    name_rePattern = "\p{L}*\s\p{Lu}{1}\p{L}+(?:[- ])?\p{Lu}{1}\p{L}+"
+
+    if regex.match(pattern=name_rePattern, string=name):
+
+        raise exceptions.ValidationError("Введіть ім'я коректно")
+
+    return name
+
+
+def check_journal(journal):
+
+    journal_rePattern = "(?!0)\d{3}"
+
+    if regex.match(pattern=journal_rePattern, string=journal):
+
+        raise exceptions.ValidationError("Введіть номер взводу коректно")
+
+    return journal
+
+
+def check_ordinal(ordinal):
+
+    ordinal_rePattern = "(?!0)\d{1,2}"
+
+    if regex.match(pattern=ordinal_rePattern, string=ordinal):
+
+        raise exceptions.ValidationError("Введіть номер коректно")
+
+    return ordinal
