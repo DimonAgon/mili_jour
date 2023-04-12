0e8905623df05c9e7bc43c85f58e59fc4dc0fca2
DimonAgon <dimon2003agon@gmail.com>
2023-04-12 14:46:57 +0300

    add registered external id filter

--- mili_jourBot/bot/handlers/filters.py
+++ mili_jourBot/bot/handlers/filters.py
@@ -1,4 +1,17 @@
+from aiogram.filters.base import Filter
+from aiogram import types
+from typing import Type
+from django.db import models
 
 
+class RegisteredExternalIdFilter(Filter):
+    key = "in_db"
 
+    async def __call__(self, model: Type[models.Model], message: types.Message, chat_mode: bool = False) -> bool:
+
+        if chat_mode:
+            id_ = message.chat.id
+        else:
+            id_ = message.from_user.id
+        return model.objects.filter(external_id=id_).exists()
 
