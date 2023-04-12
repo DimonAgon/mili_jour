6f97a3006fb44c983ee159b2cc5c86539924fc05
DimonAgon <dimon2003agon@gmail.com>
2023-04-13 00:07:28 +0300

    change models field ordering, add meta to journal

--- mili_jourBot/bot/models.py
+++ mili_jourBot/bot/models.py
@@ -1,14 +1,13 @@
-#TODO: add an "authorized" field
+# TODO: add an "authorized" field
 
 from django.db import models
 from django.urls import reverse_lazy
 
 
 class Profile(models.Model):
-
     name = models.CharField(verbose_name="Ім'я, Прізвище", max_length=65)
-    journal = models.ForeignKey(to='Journal', on_delete=models.CASCADE)
     ordinal = models.CharField(verbose_name="Номер в списку", max_length=2)
+    journal = models.ForeignKey(to='Journal', on_delete=models.CASCADE, verbose_name="Журнал")
     external_id = models.PositiveIntegerField(verbose_name='Telegram id')
 
     def get_absolute_url(self):
@@ -19,18 +18,24 @@ class Profile(models.Model):
 
     class Meta:
         verbose_name = "Профіль"
-        ordering = ['-name']
-
+        ordering = ['name']
 
 
 class Journal(models.Model):
-    external_id = models.PositiveIntegerField(verbose_name="Chat id")
     name = models.CharField(verbose_name="Номер взводу", max_length=3, db_index=True)
     strength = models.CharField(verbose_name="Чисельність взводу", max_length=2)
+    external_id = models.PositiveIntegerField(verbose_name="Chat id")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Взвод"
+        verbose_name_plural = "Взводи"
+        ordering = ['-name']
 
 
 class JournalEntry(models.Model):
     date = models.DateField()
     name = models.ForeignKey(to='Profile', on_delete=models.CASCADE)
     journal = models.CharField(max_length=180)
-
