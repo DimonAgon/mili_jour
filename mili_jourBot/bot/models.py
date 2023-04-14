eb6f0882ed62bc297b604b2b90159931b21d347d
DimonAgon <dimon2003agon@gmail.com>
2023-04-14 13:38:29 +0300

    change name field to profile field at JournalEntry

--- mili_jourBot/bot/models.py
+++ mili_jourBot/bot/models.py
@@ -28,7 +28,7 @@ class Journal(models.Model):
 
 class JournalEntry(models.Model):
     date = models.DateField()
-    name = models.ForeignKey(to='Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey(to='Profile', on_delete=models.CASCADE)
     lesson = models.IntegerField()
     journal = models.CharField(max_length=180)
     status = models.BooleanField(verbose_name="Присутність")
