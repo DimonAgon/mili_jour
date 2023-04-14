ee5a2fcb0da7ed16139def88a31302a5e12b00f7
DimonAgon <dimon2003agon@gmail.com>
2023-04-15 01:21:25 +0300

    fix hanlers to be ready to merge

--- mili_jourBot/bot/handlers/handlers.py
+++ mili_jourBot/bot/handlers/handlers.py
@@ -78,78 +78,10 @@ async def register_journal_command(message: types.Message, forms: FormsManager):
     await forms.show('journalform')
 
 
-
-
-
-
-# @router.message(JournalForm.name)#,CurrentUserFilter)
-# async def handle_registered_journal_name(message: types.Message, state: FSMContext):
-#     name = message.text
-#
-#     if not Journal.objects.get(name=name):
-#         await state.update_data(name=name)
-#         await state.set_state('strength')
-#         await message.reply(JournalForm.strength_label)
-#     else:
-#         await message.reply(text="Помилка: За заданим взводом вже cтворено журнал", disable_notification=True)
-
-
-
-# @router.message(JournalForm.strength)# ,CurrentUserFilter)
-# async def handle_registered_journal_strength(message: types.Message, state: FSMContext):
-#     strength = message.text
-#
-#     await state.update_data(strength=strength)
-#     await state.set_state('initial')
-
-
-
-# @router.message(Command(commands='register'),JournalForm.initial)  # ,CurrentUserFilter)
-# async def register_journal(message: types.message, state: FSMContext):
-#
-#         group_id = await state.get_state('group_id')
-#         name = await state.get_data('name')
-#         strength = await state.get_data('strength')
-#
-#         await message.reply(add_journal(state_name, group_id))
-#         await state.clear()
-#
-#     else:
-#         await message.reply(text="Помилка: За заданим взводом вже cтворено журнал", disable_notification=True)
-
-
-
 @router.message(Command(commands='terminate_reg'))
 async def terminate_registration_command(message: types.Message, state: FSMContext):
     await state.clear()
     await message.reply(text="Процес реєстрації було перервано")
-
-
-
-# async def last(message:types.Message):
-#
-#     entries = JournalEntry.objects.filter(date=date)
-#     # entries_list = list(map(lambda x: x.))
-#     # attendance_list =
-#
-#     # await message.reply(entry.)
-#
-# dp.poll_handler()
-
-
-
-# async def on_date(message:types.Message, date):
-#
-#     await message.reply()
-
-
-
-# @dp.poll_answer_handler()
-# async def presence_poll_answer_handler(poll_answer:types.PollAnswer):
-#     answer_id = poll_answer.option_ids
-#     user_id = poll_answer.user.id
-#     poll_id = poll_answer.poll_id
-#     entry = JournalEntry(date=date.today(), journal_id=types.chat.Chat.id
 
 
 #TODO: craete a chat leave command
