89f8574342618156855fd9eb4f31546bf16ebe3d
DimonAgon <dimon2003agon@gmail.com>
2023-05-04 23:00:12 +0300

    fix validationerror bug

--- mili_jourBot/bot/models.py
+++ mili_jourBot/bot/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse_lazy
-from django.core.validators import *
+from django.core.validators import validate_comma_separated_integer_list
 
 
 class Profile(models.Model):
