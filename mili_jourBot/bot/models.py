2a3272767d6fc631d4fae4f35f9b0b6246315061
DimonAgon <dimon2003agon@gmail.com>
2023-08-04 15:54:07 +0300

    remove TODOs

--- mili_jourBot/bot/models.py
+++ mili_jourBot/bot/models.py
@@ -20,7 +20,7 @@ class Profile(models.Model):
     class Meta:
         verbose_name = "Профіль"
         verbose_name_plural = "Взводи"
-        ordering = ['ordinal'] #TODO: oredering by ordinal is not working
+        ordering = ['ordinal']
 
 
 class Journal(models.Model):
