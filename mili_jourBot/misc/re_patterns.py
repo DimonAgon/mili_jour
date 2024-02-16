4dc14edddc031e0e10f97db67b1616e907e170de
DimonAgon <dimon2003agon@gmail.com>
2024-02-16 17:53:15 +0200

    add fullname passing possibility to report_redo

--- mili_jourBot/misc/re_patterns.py
+++ mili_jourBot/misc/re_patterns.py
@@ -7,5 +7,5 @@ journal_rePattern = "(?!0)\d{3}"
 ordinal_rePattern = "(?!0)\d{1,2}"
 sterngth_rePattern = "(?!0)\d{2}"
 report_headers_rePattern = "\s*(Студент)\s+((\d)(?:\s+(\d))+)"
-report_row_rePattern = "({})\s+((н|·|_)(?:\s+(н|·|_))+)".format(name_rePattern)
+report_row_rePattern = "({}|{})\s+((н|·|_)(?:\s+(н|·|_))+)".format(name_rePattern, full_name_rePattern)
 report_rePattern = "{}\s+({}\s*)+".format(report_headers_rePattern, report_row_rePattern)
